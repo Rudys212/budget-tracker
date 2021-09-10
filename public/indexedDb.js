877ea123b4010060f8c7ce0886bf1df2fd85215a
Rudy Sanchez <rudys212@gmail.com>
2021-09-10 01:31:26 -0700

    added indexedDB

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,48 +1,40 @@
+const request = indexedDB.open("budget", 1);
+
 let db;
 
-export function checkForIndexedDb() {
-  if (!window.indexedDB) {
-    console.log("Your browser doesn't support a stable version of IndexedDB.");
-    return false;
-  }
-  return true;
-}
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore("budgetStore", { autoIncrement: true });
+};
 
-export function useIndexedDb(databaseName, storeName, method, object) {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(databaseName, 1);
-    tx, store;
+r;
+function checkingDb() {
+  const transaction = db.transaction(["budgetStore"], "readwrite");
+  const store = transaction.objectStore("budgetStore");
+  const request = store.getAll();
 
-    request.onupgradeneeded = function (e) {
-      const db = request.result;
-      db.createObjectStore(storeName, { keyPath: "_id" });
-    };
+  request.onsuccess = function (e) {
+    db = e.request.result;
 
     request.onerror = function (e) {
       console.log("There was an error");
     };
 
-    request.onsuccess = function (e) {
-      db = request.result;
-      tx = db.transaction(storeName, "readwrite");
-      store = tx.objectStore(storeName);
-
-      db.onerror = function (e) {
-        console.log("error");
-      };
-      if (method === "put") {
-        store.put(object);
-      } else if (method === "get") {
-        const all = store.getAll();
-        all.onsuccess = function () {
-          resolve(all.result);
-        };
-      } else if (method === "delete") {
-        store.delete(object._id);
-      }
-      tx.oncomplete = function () {
-        db.close();
-      };
+    db.onerror = function (e) {
+      console.log("error");
     };
-  });
+    if (method === "put") {
+      store.put(object);
+    } else if (method === "get") {
+      const all = store.getAll();
+      all.onsuccess = function () {
+        resolve(all.result);
+      };
+    } else if (method === "delete") {
+      store.delete(object._id);
+    }
+    transaction.oncomplete = function () {
+      db.close();
+    };
+  };
 }
